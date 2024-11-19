e21635603e9b2c4c029940a82f9c14ba6a30bccf
GameNuaMua <gamenuamua333@gmail.com>
2024-11-19 16:27:12 +0700

    luu lan 3

--- test.cpp
+++ test.cpp
@@ -10,7 +10,7 @@ int main()
     // git add . : là add tất cả các file lên để chuẩn bị lưu
     // git add <tenfile.đuôi> : là add file đó lên để chuẩn bị lưu
     // git reset : để lấy các file ra khỏi repo 
-    ???
+    // git log : xem lại những lần thay đổi 
 
     getch();
     return 0;
