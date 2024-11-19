8f564826b16981a62d6e535992b77e4399800847
GameNuaMua <gamenuamua333@gmail.com>
2024-11-19 16:48:54 +0700

    luu lan 5

--- test.cpp
+++ test.cpp
@@ -14,9 +14,14 @@ int main()
     // git log : xem lại những lần thay đổi 
     // git log --oneline : xem lại những lần thay đổi (ngắn hơn)
     // git checkout <idcomment> : quay lại comment trước đó
-    // git checkout <branch name> : quay lại cái vừa xoá
+    // git checkout <branch name> : đi đến branch <branch name>
     // git branch : xem tên của branch
     // git checkout -b <ten branch> : thoat branch hiện tại và tạo ra một branch mới với <tên branch>
+    //----------------------------------
+    // + tổng hợp branch lại với nhau 
+    // trước tiên : và branch muốn lưu các file vào
+    // git merge <ten branch can gọp lại>
+    // vd : đang ở branch master vè gõ lệnh "git merge dev" thì dev sẽ được gọp vào master
 
     getch();
     return 0;
