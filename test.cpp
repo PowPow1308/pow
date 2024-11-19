a5096769e33770a7dea68b23971d374466d60dd7
GameNuaMua <gamenuamua333@gmail.com>
2024-11-19 16:21:41 +0700

    luu lan 2

--- test.cpp
+++ test.cpp
@@ -4,6 +4,14 @@
 int main()
 {
     printf("hello githup\n");
+
+    // git init : đưa folde thì repo
+    // git status : xem trạng thái của repo
+    // git add . : là add tất cả các file lên để chuẩn bị lưu
+    // git add <tenfile.đuôi> : là add file đó lên để chuẩn bị lưu
+    // git reset : để lấy các file ra khỏi repo 
+    ???
+
     getch();
     return 0;
 }
